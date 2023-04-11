5875bb0ea5471a213eabe730e33766934c23f42b
Sanskar Jadhav <113002227+SanskarJadhav@users.noreply.github.com>
2023-04-11 23:20:04 +0530

    Delete Shape.java

--- Shape.java
+++
@@ -1,9 +0,0 @@
-// creating an abstract class
-abstract public class Shape {
-    // predefined method common for all classes
-    public void stateShape(String shape){
-        System.out.println("The shape is: "+shape);
-    }
-    // abstract method to be overwritten
-    abstract public void calculateArea();
-}
